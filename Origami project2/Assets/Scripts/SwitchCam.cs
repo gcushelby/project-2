3f3f21337af1306e5f50745f21c7f9e3a307d341
shelby <ssanchez34@my.gcu.edu>
2018-09-12 09:50:56 -0700

    pro 2

---
+++ Origami project2/Assets/Scripts/SwitchCam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCam : MonoBehaviour
+{
+
+    Camera cam1, cam2;
+
+    bool IsCam1 = true;
+
+    void Start()
+    {
+        cam1 = GameObject.Find("CameraOne").GetComponent<Camera>();
+        cam2 = GameObject.Find("CameraTwo").GetComponent<Camera>();
+
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    void Update()
+    {
+        //Press the L Button to switch cameras
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space key was pressed");
+            if (IsCam1)
+            {
+                cam1.enabled = false;
+                cam2.enabled = true;
+                IsCam1 = false;
+            }
+            else
+            {
+                cam1.enabled = true;
+                cam2.enabled = false;
+                IsCam1 = true;
+            }
+        }
+    }
+}
